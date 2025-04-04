e6253c9f4f6577076facf05669b5dea632da765d
Manjunath-Aloji <manjualoji066@gmail.com>
2025-04-04 17:26:06 +0530

    nothing to do

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import { ApolloServer } from "@apollo/server";
-import {
-    ApolloServerPluginLandingPageLocalDefault,
-    ApolloServerPluginLandingPageProductionDefault,
-} from "@apollo/server/plugin/landingPage/default";
+// import {
+//     ApolloServerPluginLandingPageLocalDefault,
+//     ApolloServerPluginLandingPageProductionDefault,
+// } from "@apollo/server/plugin/landingPage/default";
 import mercury from "@mercury-js/core";
 import { mergeTypeDefs, mergeResolvers } from "@graphql-tools/merge";
 import "./models/index.ts";
@@ -25,14 +25,14 @@ const server = new ApolloServer({
     introspection: true,
     typeDefs: mergedTypeDefs,
     resolvers: mergedResolvers,
-    plugins: [
-        process.env.NODE_ENV === "production"
-            ? ApolloServerPluginLandingPageProductionDefault({
-                  graphRef: "My-Graph-dh3i1@my-graph-variant",
-                  embed: true,
-              })
-            : ApolloServerPluginLandingPageLocalDefault({ footer: false }),
-    ],
+    // plugins: [
+    //     process.env.NODE_ENV === "production"
+    //         ? ApolloServerPluginLandingPageProductionDefault({
+    //               graphRef: "My-Graph-dh3i1@my-graph-variant",
+    //               embed: true,
+    //           })
+    //         : ApolloServerPluginLandingPageLocalDefault({ footer: false }),
+    // ],
 });
 
 export default server;
